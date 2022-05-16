f3cac6be34652c3978b37c2df056c7e81e524d12
AnastasiiaNehriienko <nastianegrienko@gmail.com>
2022-05-16 13:17:25 +0300

    Add project files.

---
+++ Lab4_Py/Classes.py
@@ -0,0 +1,62 @@
+﻿class Date:
+    def __init__(self, year, month, day):
+        self.year=year 
+        self.month=month 
+        self.day=day 
+    def show(self):
+        print("Date:%02d/%02d/%4d"%(self.day, self.month, self.year))
+class Thing:
+    def __init__(self, name, made_y, made_m, made_d, price, amount):
+        self.name=name 
+        self.made=Date(made_y, made_m, made_d)
+        self.price=price 
+        self.amount=amount 
+    def all_price(self):
+        return self.price*self.amount
+    def check_date(self, how_long):
+        d=self.made.day+how_long.day
+        m=self.made.month+how_long.month+d//31
+        y=self.made.year+how_long.year+m//13
+        d1=d%30
+        m1=m%12
+        if d%30==0:
+            d1=30
+        if m%12==0:
+            m1=12
+        good=Date(y, m1, d1)
+        return good 
+    def show1(self):
+        print(self.name)
+        self.made.show()
+        print("Price:"+str(self.price)+"; Amount:"+str(self.amount))
+class Prom(Thing):
+    def __init__(self, name, made_y, made_m, made_d, price, amount, transport, place):
+        super().__init__(name, made_y, made_m, made_d, price, amount)
+        self.transport=transport 
+        self.place=place 
+    def show(self):
+        super().show1()
+        print("Transporting way:"+self.transport)
+        print("Place to store:", end="")
+        if self.place==0:
+            print("Stock")
+        else:
+            print("Shop")
+class Eat(Thing):
+    def __init__(self, name, made_y, made_m, made_d, price, amount, long_y, long_m, long_d):
+        super().__init__(name, made_y, made_m, made_d, price, amount)
+        self.how_long=Date(long_y, long_m, long_d)
+    def is_bad(self, today):
+        bad=True
+        good_day=super().check_date(self.how_long) 
+        if today.year<good_day.year:
+            bad=False 
+        elif today.year==good_day.year and today.month<good_day.month:
+            bad=False
+        elif today.year==good_day.year and today.month==good_day.month and today.day<=good_day.day:
+            bad=False
+        return bad
+    def show(self):
+        super().show1()
+        print("You can store this product", self.how_long.year, "years,", self.how_long.month, "months and",
+              self.how_long.day, "days")
---
+++ Lab4_Py/Funcs.py
@@ -0,0 +1,49 @@
+﻿import Classes
+def create_proms(x, prom):
+    for i in range(x):
+        name=input("Enter the name:")
+        made_y=int(input("Enter the date when the product was made. Year:"))
+        made_m=int(input("Month:"))
+        made_d=int(input("Day:"))
+        price=int(input("Enter the price:"))
+        amount=int(input("Enter the amount:"))
+        transport=input("What is the way of thansporting? ")
+        place=int(input("Where to store it? Press 0 for stock and 1 for shop: "))
+        thing=Classes.Prom(name, made_y, made_m, made_d, price, amount, transport, place)
+        prom.append(thing)
+    for j in range(x):
+        prom[j].show()
+def create_eats(x, eats):
+    for i in range(x):
+        name=input("Enter the name:")
+        made_y=int(input("Enter the date when the product was made. Year:"))
+        made_m=int(input("Month:"))
+        made_d=int(input("Day:"))
+        price=int(input("Enter the price:"))
+        amount=int(input("Enter the amount:"))
+        long_y=int(input("Enter how long can you store it. Years:"))
+        long_m=int(input("Months:"))
+        long_d=int(input("Days:"))
+        thing=Classes.Eat(name, made_y, made_m, made_d, price, amount, long_y, long_m, long_d)
+        eats.append(thing)
+    for j in range(x):
+        eats[j].show()
+def bad_products(x, eats):
+    sum=0
+    y=int(input("What date is it today? Year:"))
+    m=int(input("Month:"))
+    d=int(input("Day:"))
+    today=Classes.Date(y, m, d)
+    for i in range(x):
+        if eats[i].is_bad(today)==True:
+            sum+=eats[i].all_price()
+    return sum
+def stock_products(x, proms):
+    sum=0
+    for i in range(x):
+        if proms[i].place==0:
+            sum+=proms[i].all_price()
+    return sum
+def show(bads, stock):
+    print("The total price of bad products is:", bads)
+    print("The total price of stock products is:", stock)
